76201e06a3c4288ac0f166649e018d3984944e53
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-12-20 16:46:57 -0600

    el primer commit lcm-hero

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <section id="hero">
+      <div className="container">
+        <div className="info">
+          <h1>Expo Navidad 2021</h1>
+          <h2>Encuentra productos, comida,accesorios, los mejores paquetes ,los mejores regalos para toda la familia</h2>
+          <p>
+            El mejor regalo para la familia y los seres queridos en esta temporada Navidña y de Año Nuevo.
+          </p>
+          <button>
+            <a href="https://www.youtube.com/watch?v=nM6y5V7oIuE">
+              Click al videoclip
+            </a>
+          </button>
+        </div>
+      </div>
+    </section>
+ 
+  );
+}
+
+export default App;
